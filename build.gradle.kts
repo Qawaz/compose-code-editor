3962222917c9cc3fbf8b80ca97adf3c6d78767cd
Waqas Tahir <wakaztahir@gmail.com>
2023-04-27 17:49:15 +0500

    code

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
     id("com.android.application").apply(false)
     id("com.android.library").apply(false)
     id("org.jetbrains.compose").apply(false)
+    id("maven-publish")
     id("org.jetbrains.dokka")
 }
 
@@ -20,10 +21,34 @@ tasks.withType<org.jetbrains.dokka.gradle.DokkaMultiModuleTask>(){
     outputDirectory.set(rootProject.file("docs/api"))
 }
 
+fun RepositoryHandler.githubPackages() {
+    maven("https://maven.pkg.github.com/Qawaz/compose-code-editor") {
+        name = "GithubPackages"
+        try {
+            credentials {
+                username = (System.getenv("GPR_USER")).toString()
+                password = (System.getenv("GPR_API_KEY")).toString()
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+    }
+}
+
+subprojects {
+    apply(plugin = "maven-publish")
+    publishing {
+        repositories {
+            githubPackages()
+        }
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        githubPackages()
     }
 }
